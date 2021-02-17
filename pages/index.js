397f8ee31ff7342c38d3149746736532ec22b9ab
Cangemi <65517004+Cangemi@users.noreply.github.com>
2021-02-17 01:51:38 -0300

    página fotografia

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import Head from 'next/head';
+
+
 import Navbar from '../components/Navbar';
 
 import { Jumbotron, Container } from 'reactstrap'
@@ -5,18 +8,32 @@ import { Jumbotron, Container } from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function Home(){
-    return(
+function Home() {
+    return (
         <div>
+            <Head>
+
+                <title>Pagina de estudo</title>
+                <meta name='description' content='Site de estudo' />
+                <meta name='author' content='Pedro Cangemi' />
+                <link href="https://fonts.googleapis.com/css2?family=Chau+Philomene+One&display=swap" rel="stylesheet"></link>
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet"></link>
+
+            </Head>
             <Navbar />
             <Jumbotron fluid id="Cabecalho">
                 <Container >
-                    <div class ="row">  
-                        <div  class="col">
-                            <h1 className="Text1">Não registramos apenas momentos.</h1>
+                    <div class="row">
+                        <div class="col">
+                            <h1 className="Titulo1">Não registramos <br />apenas momentos.</h1>
+                            <h2 className="Divisor">▀ ▀ ▀ ▀ ▀ ▀ ▀ ▀ ▀ ▀ ▀ ▀ ▀ ▀</h2>
+                            <h2 className="Titulo2" >Eternizamos emoções</h2>
+                            <div  className="BtnHead">
+                             <button className="BtnHead2" href="/">Orçamento</button>
+                            </div>
                         </div>
                         <div class="col" >
-                            <img src="/header.png" width="620" height="420" />
+                            <img src="/header.png" width="620" height="480" />
                         </div>
                     </div>
 
